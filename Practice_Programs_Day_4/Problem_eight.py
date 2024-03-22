be4768a91e1af905879b4be5ebcb378e00239b00
vinithmh <vinithmh2000@gmail.com>
2024-03-22 10:33:32 +0530

    file changes

--- Practice_Programs_Day_4/Problem_eight.py
+++ Practice_Programs_Day_4/Problem_eight.py
@@ -1,3 +1,12 @@
+"""
+8. Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+Suppose the following input is supplied to the program:
+Hello world!
+Then, the output should be:
+UPPER CASE 1
+LOWER CASE 9
+"""
+
 def count_case(sentence):
     upper_count = 0
     lower_count = 0
